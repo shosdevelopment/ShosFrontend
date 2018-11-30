38d3be79d4ecfb1ec3318afc3631675f7afe6b17
Moshe <mmoshikoo@gmail.com>
2018-11-30 14:05:10 +0200

    Finished working on navigation bar with responsive and beautiful design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BasePageAbstract } from './Abstracts/base-page.abstract';
+import { Router } from '@angular/router';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent extends BasePageAbstract implements OnInit {
+  //#region Constructor
+  public constructor(protected router: Router) {
+    super(router);
+  }
 
+  public ngOnInit(): void {
+    $(document).ready(function () {
+      $('.menu-icon').on('click', function () {
+        $('nav ul').toggleClass('showing');
+      });
+    });
+
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > 70) {
+        $('nav').addClass('black');
+      } else {
+        $('nav').removeClass('black');
+      }
+    });
+  }
+  //#endregion
 }
+
